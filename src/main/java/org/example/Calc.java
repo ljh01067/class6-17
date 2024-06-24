8332aa212a75b1db17c8e02bc88eb719036b1af1
ljh01067 <leejh01067@gmail.com>
2024-06-24 16:36:37 +0900

    0624-2

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,19 +1,12 @@
 package org.example;
 
-public class Calc{
-    public static int run(String exp){
+public class Calc {
+    public static int run(String exp) {
         int i = 0;
-        if(exp.indexOf("+") != -1){
-            String[] bits = exp.split("\\+");
-            int a = Integer.parseInt(bits[0]);
-            int b = Integer.parseInt(bits[1]);
-            i = a+b;
-        }
-        else if(exp.indexOf("-") != -1){
-            String[] bits = exp.split("-");
-            int a = Integer.parseInt(bits[0]);
-            int b = Integer.parseInt(bits[1]);
-            i = a-b;
+        exp = exp.replace("-", "+-");
+        String[] bits = exp.split("\\+");
+        for (int j = 0; j < bits.length; j++) {
+            i += Integer.parseInt(bits[j]);
         }
         return i;
     }
