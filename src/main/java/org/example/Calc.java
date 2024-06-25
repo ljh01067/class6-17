89d59ca03782d0168847838f66dddac5a551b1e9
ljh01067 <leejh01067@gmail.com>
2024-06-25 16:47:34 +0900

    0625

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,12 +1,81 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Calc {
     public static int run(String exp) {
         int i = 0;
-        exp = exp.replace("-", "+-");
-        String[] bits = exp.split("\\+");
-        for (int j = 0; j < bits.length; j++) {
-            i += Integer.parseInt(bits[j]);
+        //수식에 곱하기가 있는경우
+        if(exp.indexOf(" * ") != -1) {
+            i = 1;
+            //수식에 곱하기와 더하기가 있는 경우
+            if (exp.indexOf(" + ") != -1) {
+                int unum = 0;
+                int dnum = 0;
+                int f = 0;
+                //+를 기준으로 나눈뒤 나눈것들 사이에서 *가 있는 것의 위치를 찾는다.
+                String[] bits = exp.split(" \\+ ");
+                for (int l = 0; l < bits.length; l++) {
+                    if (bits[l].contains("*")) {
+                        unum++;
+                    }
+                    for (l = 0; l < bits.length; l++) {
+                        if (bits[l].contains("*") == false) {
+                            dnum++;
+                        }
+                    }
+                    int[] n = new int[unum];
+                    int[] g = new int[dnum];
+                    for (int k = 0; k < bits.length; k++) {
+                        for (l = 0; l < unum; l++) {
+                            if (bits[k].contains("*")) {
+                                n[l] = k;
+                            }
+                        }
+                        for (l = 0; l < dnum; l++) {
+                            if (bits[k].contains("*") == false) {
+                                g[l] = k;
+                            }
+                        }
+                    }
+                    //위치를 기반으로 *를 기준으로 나누고 곱한다.
+                    int ssum = 0;
+                    int[] sum = new int[unum];
+                    for (int m = 0; m < unum; m++) {
+                        i = 1;
+                        String[] bits2 = bits[n[m]].split(" \\* ");
+                        for (int j = 0; j < bits2.length; j++) {
+                            i *= Integer.parseInt(bits2[j]);
+                            sum[m] = i;
+                        }
+                            ssum += sum[m];
+                    }
+                    for (int m = 0; m < dnum; m++) {
+                        f += Integer.parseInt(bits[g[m]]);
+                    }
+                    i = ssum + f;
+                }
+            }
+            //수식에 곱하기만 있는경우
+            else {
+                String[] bits = exp.split(" \\* ");
+                for (int j = 0; j < bits.length; j++) {
+                    i *= Integer.parseInt(bits[j]);
+                }
+            }
+        }
+        else if(exp.indexOf(" - ") != -1) {
+            exp = exp.replace(" - ", " + -");
+            String[] bits = exp.split(" \\+ ");
+            for (int j = 0; j < bits.length; j++) {
+                i += Integer.parseInt(bits[j]);
+            }
+        }
+        else if(exp.indexOf(" + ") != -1) {
+            String[] bits = exp.split(" \\+ ");
+            for (int j = 0; j < bits.length; j++) {
+                i += Integer.parseInt(bits[j]);
+            }
         }
         return i;
     }
--- src/test/java/org/example/Tests.java
+++ src/test/java/org/example/Tests.java
@@ -7,43 +7,68 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Tests {
     @Test
-    @DisplayName("1+1 = 2")
+    @DisplayName("1 + 1 = 2")
     void t1(){
-        assertThat(Calc.run("1+1")).isEqualTo(2);
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
     }
     @Test
-    @DisplayName("2+1 = 3")
+    @DisplayName("2 + 1 = 3")
     void t2(){
-        assertThat(Calc.run("2+1")).isEqualTo(3);
+        assertThat(Calc.run("2 + 1")).isEqualTo(3);
     }
     @Test
-    @DisplayName("2+2 = 3")
+    @DisplayName("2 + 2 = 3")
     void t3(){
-        assertThat(Calc.run("2+2")).isEqualTo(4);
+        assertThat(Calc.run("2 + 2")).isEqualTo(4);
     }
     @Test
-    @DisplayName("1000+200 = 1200")
+    @DisplayName("1000 + 200 = 1200")
     void t4(){
-        assertThat(Calc.run("1000+200")).isEqualTo(1200);
+        assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
     }
     @Test
-    @DisplayName("50-30 = 20")
+    @DisplayName("50 - 30 = 20")
     void t5(){
-        assertThat(Calc.run("50-30")).isEqualTo(20);
+        assertThat(Calc.run("50 - 30")).isEqualTo(20);
     }
     @Test
-    @DisplayName("10+20+30 = 60")
+    @DisplayName("10 + 20 + 30 = 60")
     void t6(){
-        assertThat(Calc.run("10+20+30")).isEqualTo(60);
+        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
     }
     @Test
-    @DisplayName("10-20+30 = 20")
+    @DisplayName("10 - 20 + 30 = 20")
     void t7(){
-        assertThat(Calc.run("10-20+30")).isEqualTo(20);
+        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
     }
     @Test
-    @DisplayName("10-10-10-10+10+10-10 = -10")
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 = -10")
     void t8(){
-        assertThat(Calc.run("10-10-10-10+10+10-10")).isEqualTo(-10);
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
+    @Test
+    @DisplayName("10 * 10 * 10 = 1000")
+    void t9(){
+        assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
+    }
+    @Test
+    @DisplayName("10 + 5 * 2 = 20")
+    void t10(){
+        assertThat(Calc.run("10 + 5 * 2")).isEqualTo(20);
+    }
+    @Test
+    @DisplayName("20 + 10 + 5 * 2 = 40")
+    void t11(){
+        assertThat(Calc.run("20 + 10 + 5 * 2")).isEqualTo(40);
+    }
+    @Test
+    @DisplayName("20 + 10 * 5 + 10 = 80")
+    void t12(){
+        assertThat(Calc.run("20 + 10 * 5 + 10")).isEqualTo(80);
+    }
+    @Test
+    @DisplayName("10 * 20 + 10 * 5 = 250")
+    void t13(){
+        assertThat(Calc.run("10 * 20 + 10 * 5")).isEqualTo(250);
     }
 }
